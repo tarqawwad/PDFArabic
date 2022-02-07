ed0bab8465af7f989b9e8873697f28263fffd722
Tareq Awwad <Tareq Awwad@DELLI711G>
2022-02-07 11:40:17 -0800

    Add project files.

---
+++ PDFArabic/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Rotativa;
+using Rotativa.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PDFArabic.Controllers
+{
+    [RoutePrefix("Home")]
+    public class HomeController : Controller
+    {
+        //By Tareq Awwad
+        public ActionResult Index()
+        {
+            return View(PrepareData());
+        }
+
+        public List<PDFArabic.Models.StudentModel> PrepareData()
+        {
+            var Model = new List<PDFArabic.Models.StudentModel>();
+           for(int i = 1; i <= 10; i++)
+            {
+                Model.Add(new Models.StudentModel
+                {
+                    StudentName =  "محمد أحمد" + i,
+                    StudentDepartment = "علم حاسوب",
+                    StudentAge = i/2==0?22:23,
+                    StudentAVG = i+1,
+                    StudentNumber = i
+                });
+            }
+            return Model;
+        }
+
+        [HttpGet]
+        [Route("DownloadPDF",Name = "DownloadPDF")]
+        public ActionResult DownloadPDF()
+        {
+            string fileName = $"" + "Students" + ".pdf";
+
+            return new ViewAsPdf("~/Views/Shared/ReportStudent.cshtml", PrepareData())
+            {
+                FileName = fileName,
+                PageOrientation = Orientation.Portrait,
+                PageSize = Rotativa.Options.Size.A4,
+                PageMargins = new Rotativa.Options.Margins { Left = 0, Right = 0, Top = 0, Bottom = 0 },
+            };
+        }
+        //By Tareq Awwad
+    }
+}
---
+++ PDFArabic/Models/StudentModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PDFArabic.Models
+{
+    public class StudentModel
+    {
+        public int StudentNumber { get; set; }
+        public string StudentName { get; set; }
+        public int StudentAge { get; set; }
+        public string StudentDepartment { get; set; }
+        public double StudentAVG { get; set; }
+    }
+}
